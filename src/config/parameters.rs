018c420e28db8dd42580fb81f63d6611a9fd158e
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-01-30 11:54:43 +0300

    upd

--- src/config/parameters.rs
+++ src/config/parameters.rs
@@ -12,10 +12,10 @@ pub const QUOTE_LIST: &[(&str, f64)] = &[
 pub const QUOTE_RATE: f64 = 2.0;
 pub const REQUIRED_TICKER_SPREAD_PERCENT: f64 = 0.25;
 pub const REQUIRED_ORDERBOOK_SPREAD_PERCENT: f64 = 0.3;
-pub const NETWORKS_CACHE_TTL_SECS: u64 = 600;
-pub const MARGIN_INFO_CACHE_TTL_SECS: u64 = 600;
+pub const NETWORKS_CACHE_TTL_SECS: u64 = 1200;
+pub const MARGIN_INFO_CACHE_TTL_SECS: u64 = 1200;
 
-pub const REQUESTS_CHUNK_SIZE: usize = 3;
+pub const REQUESTS_CHUNK_SIZE: usize = 10;
 
 pub const HTTP_MAX_POOL_IDLE_PER_HOST: usize = 20;
 pub const HTTP_RETRY_AFTER_MILLIS: u64 = 300;
