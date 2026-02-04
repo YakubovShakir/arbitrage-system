4301733052557b24cdc3a7c6e62b3264e1070105
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-02-04 08:55:52 +0300

    add networks

--- src/init/exchanges.rs
+++ src/init/exchanges.rs
@@ -39,7 +39,7 @@ pub fn get_exchanges() -> Result<Exchanges, Box<dyn Error>> {
             ),
         },
     });
-    exchanges.insert(config::binance::NAME.to_owned(), binance);
+    // exchanges.insert(config::binance::NAME.to_owned(), binance);
 
     // Bybit
     let bybit = Exchange::Bybit(ExchangeConfig {
