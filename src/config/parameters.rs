1577832016b14e97ee32397a054ecedc8c6d1aec
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-02-13 19:21:33 +0300

    test RateLimiter

--- src/config/parameters.rs
+++ src/config/parameters.rs
@@ -47,9 +47,9 @@ pub const BITGET_REQUESTS_PER_SECOND: usize = 5;
 pub const BITMART_REQUESTS_PER_SECOND: usize = 5;
 pub const GATE_REQUESTS_PER_SECOND: usize = 450;
 pub const HUOBI_REQUESTS_PER_SECOND: usize = 100;
-pub const MEXC_REQUESTS_PER_SECOND: usize = 25;
+pub const MEXC_REQUESTS_PER_SECOND: usize = 15;
 pub const OKX_REQUESTS_PER_SECOND: usize = 20;
-pub const KUCOIN_REQUESTS_PER_SECOND: usize = 25;
+pub const KUCOIN_REQUESTS_PER_SECOND: usize = 15;
 
 // pub const BINANCE_REQUESTS_PER_SECOND: usize = 0;
 // pub const BYBIT_REQUESTS_PER_SECOND: usize = 0;
