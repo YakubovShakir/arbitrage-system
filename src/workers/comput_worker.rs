ade04115c91b3dc8619fadbaecf9c92131002555
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-02-16 15:10:15 +0300

    rework: spread table

--- src/workers/comput_worker.rs
+++ src/workers/comput_worker.rs
@@ -398,9 +398,9 @@ impl Workable for ComputWorker {
 
             for spread_pair in spread_pairs {
                 let mut message = format!("✅ {}/{} ✅", spread_pair.0.base, spread_pair.0.quote);
-                let horizontal_line = format!("{}\n", "─".repeat(125));
+                let horizontal_line = format!("{}", "─".repeat(125));
 
-                message += &("\n┌".to_owned() + &horizontal_line);
+                message += &("\n┌".to_owned() + &horizontal_line + "┐");
                 message += &format!(
                     "│ {:<14} │ {:<14} │ {:<14} │ {:<14} │ {:<14} │ {:<14} │ {:<14} │",
                     "Buy Ex",
@@ -455,7 +455,7 @@ impl Workable for ComputWorker {
                     }
 
                     let bundle_line = format!(
-                        "│{:<14}│{:<14}│{:<14}│{:<14.2}│{:<14.5}│{:<14.5}│{:<14}│",
+                        "│ {:<14} │ {:<14} │ {:<14} │ {:<14.2} │ {:<14.5} │ {:<14.5} │ {:<14} │",
                         bundle.buy_ex.config().name,
                         bundle.sell_ex.config().name,
                         bundle.quote_volume,
