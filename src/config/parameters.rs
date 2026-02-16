1e4273c5ea24e1fe8fedf77072a2032e7dbf2190
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-02-16 08:11:24 +0300

    add networks and fix rate limits

--- src/config/parameters.rs
+++ src/config/parameters.rs
@@ -42,12 +42,12 @@ pub const HUOBI_MARGIN_INFO_HTTP_TIMEOUT_SECONDS: u64 = 10;
 
 // Rate Limits
 pub const BINANCE_REQUESTS_PER_SECOND: usize = 10;
-pub const BYBIT_REQUESTS_PER_SECOND: usize = 5;
-pub const BITGET_REQUESTS_PER_SECOND: usize = 5;
-pub const BITMART_REQUESTS_PER_SECOND: usize = 5;
+pub const BYBIT_REQUESTS_PER_SECOND: usize = 10;
+pub const BITGET_REQUESTS_PER_SECOND: usize = 10;
+pub const BITMART_REQUESTS_PER_SECOND: usize = 10;
 pub const GATE_REQUESTS_PER_SECOND: usize = 450;
-pub const HUOBI_REQUESTS_PER_SECOND: usize = 100;
-pub const MEXC_REQUESTS_PER_SECOND: usize = 5;
+pub const HUOBI_REQUESTS_PER_SECOND: usize = 50;
+pub const MEXC_REQUESTS_PER_SECOND: usize = 10;
 pub const OKX_REQUESTS_PER_SECOND: usize = 20;
 pub const KUCOIN_REQUESTS_PER_SECOND: usize = 15;
 
