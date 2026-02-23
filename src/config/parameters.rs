7e741783cb1d435048ef914208898d72debd8f6f
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-02-23 12:47:50 +0300

    add base blacklist

--- src/config/parameters.rs
+++ src/config/parameters.rs
@@ -11,7 +11,7 @@ pub const QUOTE_LIST: &[(&str, f64)] = &[
 
 pub const QUOTE_RATE: f64 = 1.5;
 pub const REQUIRED_TICKER_SPREAD_PERCENT: f64 = 0.33;
-pub const REQUIRED_ORDERBOOK_SPREAD_PERCENT: f64 = 0.33;
+pub const REQUIRED_ORDERBOOK_SPREAD_PERCENT: f64 = 0.30;
 
 // cache
 pub const NETWORKS_CACHE_TTL_SECS: u64 = 12000;
