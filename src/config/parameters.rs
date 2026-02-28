b5c397d9dd5374d256fe09b602e7319d6f875f9a
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-02-28 17:07:15 +0300

    upd params

--- src/config/parameters.rs
+++ src/config/parameters.rs
@@ -53,7 +53,7 @@ pub const MEXC_REQUESTS_PER_SECOND: usize = 7;
 pub const OKX_REQUESTS_PER_SECOND: usize = 20;
 pub const KUCOIN_REQUESTS_PER_SECOND: usize = 15;
 // camp временно под блокировкой но можно и не вынимать
-pub const BASE_BLACKLIST: &[&str] = &["USTC", "LUNC", "RVN", "CAMP"];
+pub const BASE_BLACKLIST: &[&str] = &["USTC", "LUNC", "RVN", "CAMP", "LUNA"];
 // unused
 pub const SUCCESS_CODE: &str = "\x1b[38;5;48m"; // #00ff87 - яркий мятный ✅
 pub const ERROR_CODE: &str = "\x1b[38;5;203m"; // #ff5f5f - яркий алый ❌  
