f7f99dfdf1860a44dd659552645fc44dd98303a9
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-03-13 19:27:10 +0300

    debug: add futures tickers

--- src/services/futures_ticker.rs
+++ src/services/futures_ticker.rs
@@ -431,7 +431,7 @@ async fn establish_ws_connection_and_sub(
                         r#"{
                                 "id": 124,
                                 "type": "subscribe",
-                                "topic": "/contractMarket/tickerV2:all",
+                                "topic": "/contractMarket/ticker:*",
                                 "response": true
                                 }"#,
                     ))
