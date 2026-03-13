ca37702f900dcaea8ebda61fbb0b18bf11456183
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-03-13 18:31:55 +0300

    debug: add futures tickers

--- src/core/types/api.rs
+++ src/core/types/api.rs
@@ -70,7 +70,7 @@ impl API {
                 Exchange::Mexc(_) => None,
                 Exchange::Huobi(_) => Some("/linear-swap-ex/market/detail/batch_merged"),
                 Exchange::Bitmart(_) => Some("/contract/public/details"),
-                Exchange::Okx(_) => Some("/api/v5/market/tickers"),
+                Exchange::Okx(_) => Some("/api/v5/public/instruments"),
             },
             API::GetMarginInfo => match exchange {
                 Exchange::Binance(_) => Some("/sapi/v1/margin/allAssets"),
