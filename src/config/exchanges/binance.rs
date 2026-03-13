fda51e9442ddec8f7376ecc9efabeb590044d9aa
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-03-13 15:23:08 +0300

    debug: add futures tickers

--- src/config/exchanges/binance.rs
+++ src/config/exchanges/binance.rs
@@ -1,4 +1,4 @@
 pub const NAME: &str = "Binance";
 pub const BASE_URL: &str = "https://api-gcp.binance.com";
 pub const WEBSOCKET_URL: &str = "wss://stream.binance.com:9443/ws";
-pub const FUTURES_WEBSOCKET_URL: &str = "wss://fstream.binance.com";
+pub const FUTURES_WEBSOCKET_URL: &str = "wss://fstream.binance.com:9443/ws";
